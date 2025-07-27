12557fc16226c146fcc73f84a9ff28f5fa9cc9c6
Hillary Robert <roberthillary30@gmail.com>
2025-07-27 15:49:40 +0300

    Resolve a 404 page not found

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -13,6 +13,11 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+
+      <div>
+        <h1> Welcome to Splash App </h1>
+      </div>
+      
       {/* Welcome Message */}
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
         Welcome to Splash App!
